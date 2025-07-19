5799f6492cd7d20997d2b25af3237705a60eafd3
Gosho Zvqr <mamarida15@abv.bg>
2025-07-19 12:40:50 +0300

    added a rect with ibo

--- main.cpp
+++ main.cpp
@@ -30,15 +30,19 @@ int main(void)
 
     }
 
-    float positions[12] = {
+    float positions[8] = {
 
         -0.5f, -0.5f,
         -0.5f,  0.5f,
          0.5f, -0.5f,
+         0.5f,  0.5f
 
-        -0.5f,  0.5f,
-         0.5f,  0.5f,
-         0.5f, -0.5f
+    };
+
+    unsigned int indexBuffer[6] = {
+
+        0, 1, 2,
+        2, 3, 1
 
     };
 
@@ -46,15 +50,23 @@ int main(void)
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     glEnableVertexAttribArray(0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexBuffer), indexBuffer, GL_STATIC_DRAW);
+
 
     while (!glfwWindowShouldClose(window))
     {
         
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+       glDrawElements(GL_TRIANGLES, 8, GL_UNSIGNED_INT, 0);
+       //glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
 
